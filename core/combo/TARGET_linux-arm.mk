0c71f683658de26f16945868358439428f8b610e
Kyle Repinski <repinski23@gmail.com>
2016-08-27 16:16:16 -0500

    Make TARGET_LEGACY_GCC_VERSION work like TARGET_GCC_VERSION.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -38,9 +38,14 @@ $(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := 4.9
 
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
 $(combo_2nd_arch_prefix)TARGET_GCC_VERSION := 4.9
-$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := 4.8
 else
 $(combo_2nd_arch_prefix)TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
+endif
+
+ifeq ($(strip $(TARGET_LEGACY_GCC_VERSION_EXP)),)
+$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := 4.8
+else
+$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := $(TARGET_LEGACY_GCC_VERSION_EXP)
 endif
 
 TARGET_ARCH_SPECIFIC_MAKEFILE := $(BUILD_COMBOS)/arch/$(TARGET_$(combo_2nd_arch_prefix)ARCH)/$(TARGET_$(combo_2nd_arch_prefix)ARCH_VARIANT).mk
--- envsetup.sh
+++ envsetup.sh
@@ -192,6 +192,7 @@ function setpaths()
     targetgccversion2=$(get_build_var 2ND_TARGET_GCC_VERSION)
     targetlegacygccversion=$(get_build_var TARGET_LEGACY_GCC_VERSION)
     export TARGET_GCC_VERSION=$targetgccversion
+    export TARGET_LEGACY_GCC_VERSION=$targetlegacygccversion
 
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
     export ANDROID_TOOLCHAIN=
